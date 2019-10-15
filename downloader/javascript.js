51dd82db6369c02c0f33d7b8f62ada3fe4157fdc
Artem <55802327+Azurexus@users.noreply.github.com>
2019-10-15 09:13:51 +0400

    Add files via upload

--- downloader/javascript.js
+++ downloader/javascript.js
@@ -1,97 +1,80 @@
 let input = document.querySelector('#input');
 let sort = document.querySelector('.sort');
+let boxes = document.querySelector('#sortable');
 let box = document.querySelector('.ui-state-default');
 
 input.style.opacity = 0;
 
-// let image = document.createElement('img');
-// let para = document.createElement('p');
+let inputFile = document.querySelector('#input');
 
-// let image = document.createElement('img');
+function handleFiles(files) {
 
-// $('#input').change(function() {
-//     if ($(this).val() != '') $(this).prev().text('Выбрано файлов: ' + $(this)[0].files.length);
-//     else $(this).prev().text('Выберите файлы');
-// });
-// let curFiles = input.files;
-// let image = document.createElement('img');
-// let para = document.createElement('p');
-// $('#input').click(function() {
-//
-// }
+    let imageType = /image.*/;
+    for (let file of files) {
+        if (file.type.match(imageType)) {
+            let reader = new FileReader();
 
-input.addEventListener('change', imgUpdate);
+            reader.onload = function handleFiles(e) {
 
-function imgUpdate() {
-    while(box.firstChild) {
-        box.removeChild(box.firstChild);
-    }
-    while(sort.firstChild) {
-        sort.removeChild(sort.firstChild);
-    }
-    let curFiles = input.files;
-    let image = document.createElement('img');
-    let para = document.createElement('p');
-    for (var i = 0; i < curFiles.length; i++) {
-        para.textContent = 'file name ' + curFiles[i].name + ', file size ' + returnFileSize(curFiles[i].size) + '.';
-        image.setAttribute('width', '80');
-        image.src = window.URL.createObjectURL(curFiles[i]);
-        para.setAttribute('class', 'democlass');
+                let li = document.createElement("li");
+                boxes.appendChild(li);
+
+                let img = new Image();
+                img.src = reader.result;
 
 
-        box.appendChild(image);
-        sort.appendChild(para);
+                li.appendChild(img);
+                img.setAttribute("class", "images");
+                document.getElementById('progressBar').value = 0;
+                document.getElementById('progPer').innerText = '';
+
+            }
+            reader.onprogress = function handleFiles(e) {
+                let math = Math.round((e.loaded / e.total) * 100);
+                document.getElementById('progressBar').value = math;
+                document.getElementById('progPer').innerText = math + '%';
+            }
+
+            reader.readAsDataURL(file);
+        } else {
+            boxes.innerHTML = "File not supported!"
+        }
     }
 }
 
-//input.addEventListener('change', updateImageDisplay);
+inputFile.addEventListener('change', function(e) {
+    handleFiles(e.target.files)
+});
 
-// function updateImageDisplay() {
-//     while(sort.firstChild) {
-//         sort.removeChild(sort.firstChild);
-//     }
-//     let curFiles = input.files;
-//     if (curFiles === 0) {
-//         let para = document.createElement('p');
-//         para.textContent = 'not not not';
-//         sort.appendChild(para);
-//     }else {
-//         let list = document.createElement('ol');
-//         sort.appendChild(list);
-//         for (var i = 0; i < curFiles.length; i++) {
-//             let listItem = document.createElement('li');
-//             let para = document.createElement('p');
-            // if (validFileType(curFiles[i])) {
-            // para.textContent = 'file name ' + curFiles[i].name + ', file size ' + returnFileSize(curFiles[i].size) + '.';
-            // let image = document.createElement('img');
-            // image.setAttribute('width', '80');
-            // image.src = window.URL.createObjectURL(curFiles[i]);
-            //
-            // listItem.appendChild(image);
-            // listItem.appendChild(para);
+let dropbox;
 
-            // }else {
-            //     para.textContent = 'File name ' + curFiles[i].name + 'Not valid bleatb';
-            //     listItem.appendChild(para);
-            // }
-//
-//             list.appendChild(listItem);
-//         }
-//     }
-// }
-// let fileTypes = [
-//   'image/jpeg',
-//   'image/psd',
-//   'image/png'
-// ]
-// function validFileType(file) {
-//     for (var i = 0; i < fileTypes.length; i++) {
-//         if (file.type === fileTypes[i]) {
-//             return true;
-//         }
-//     }
-//     return false;
-// }
+dropbox = document.getElementById("dropbox");
+dropbox.addEventListener("dragenter", dragenter, false);
+dropbox.addEventListener("dragover", dragover, false);
+dropbox.addEventListener("drop", drop, false);
+
+function dragenter(e) {
+  e.stopPropagation();
+  e.preventDefault();
+  console.log('enter');
+}
+
+function dragover(e) {
+  e.stopPropagation();
+  e.preventDefault();
+
+}
+
+function drop(e) {
+  e.stopPropagation();
+  e.preventDefault();
+
+  let dt = e.dataTransfer;
+  let files = dt.files;
+
+  handleFiles(files);
+}
+
 function returnFileSize(number) {
   if(number < 1024) {
     return number + 'bytes';
@@ -101,22 +84,8 @@ function returnFileSize(number) {
     return (number/1048576).toFixed(1) + 'MB';
   }
 }
-// // for (var i = 0; i < curFiles.length; i++) {
-// //     let listItem = document.createElement('li');
-// //     let para = document.createElement('p');
-// //     if (validFileType(curFiles[i])) {
-// //         para.textContent = 'file name ' + curFiles[i].name + ', file size ' + returnFileSize(curFiles[i].size) + '.';
-// //         let image = document.createElement('img');
-// //         image.setAttribute('width', '80');
-// //         image.src = window.URL.createObjectURL(curFiles[i]);
-// //
-//         listItem.appendChild(image);
-//         listItem.appendChild(para);
-//
-//     }else {
-//         para.textContent = 'File name ' + curFiles[i].name + 'Not valid bleatb';
-//         listItem.appendChild(para);
-//     }
-//
-//     list.appendChild(listItem);
-// }
+
+$( function() {
+  $( "#sortable" ).sortable();
+  $( "#sortable" ).disableSelection();
+} );
